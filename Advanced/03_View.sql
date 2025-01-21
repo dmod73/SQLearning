b20fc64e652691115dc256ff77234664feffb892
dmod73 <ortizdiazdeanm@gmail.com>
2025-01-21 19:07:23 -0400

    continuacion advanced

---
+++ Advanced/03_View.sql
@@ -0,0 +1,10 @@
+Create View v_usersLicence AS
+Select Name, Age, l.LicenceNum 
+From Users as u
+Join Licence as l
+ON u.User_id = l.User_id
+WHERE Age >= 25;
+
+--como ver todas las vistas
+SELECT TABLE_NAME AS ViewName
+FROM INFORMATION_SCHEMA.VIEWS;
